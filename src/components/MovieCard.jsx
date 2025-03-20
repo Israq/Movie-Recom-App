cb75be461db88d5248c44b5b27120a7e75f848ac
Syed Ragib Israq <141021671+Israq-sebpo@users.noreply.github.com>
2025-03-20 17:27:48 +0600

    Movie Card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -11,7 +11,22 @@ const MovieCard = ({
             ? `https://image.tmdb.org/t/p/w500/${poster_path}`
             : "/no-movie.png"
         }
+        alt={title}
       />
+
+      <div className="mt-4">
+        <h3>{title}</h3>
+        <div className="content">
+          <div className="rating">
+            <img src="star.svg" alt="Star Icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+          </div>
+          <span>.</span>
+          <p className="year">
+            {release_date ? release_date.split("-")[0] : "N/A"}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
