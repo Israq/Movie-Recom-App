09ce49184cefd217bbc15e18a8fcd4da0d9913ec
Syed Ragib Israq <141021671+Israq-sebpo@users.noreply.github.com>
2025-04-09 20:59:57 +0600

    Movie-api

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MovieCard = ({
-  movie: { title, vote_average, poster_path, release_date, original_language },
+  movie: { title, vote_average, poster_path, release_date },
 }) => {
   return (
     <div className="movie-card">
